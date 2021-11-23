8d8e52a59de8d085e675583cbc7e8484cd56c470
daiking1756 <daiking1756@gmail.com>
2021-11-23 19:35:21 +0900

    Split some process to functions to make it better code

--- sketches/hanapiku/hanapiku.ino
+++ sketches/hanapiku/hanapiku.ino
@@ -13,16 +13,17 @@ CRGB leds_color[NUM_LEDS];
 
 void setup() {
   M5.begin();
-  M5.Lcd.print("Hello HANAPIKU!\n\n");
+
   Serial.begin(115200);
   pinMode(VOL_PIN, INPUT);
 
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds_color, NUM_LEDS);
   FastLED.setBrightness(20);
-
   for(int i=0; i<NUM_LEDS; i++) {
     leds_color[i] = CRGB::White;
   }
+
+  M5.Lcd.print("Hello HANAPIKU!\n\n");
 }
 
 void change_to_off_color() {
@@ -37,10 +38,12 @@ void change_to_on_color() {
   }
 }
 
-void loop() {
+void update_sensor_value() {
   raw_value = analogRead(VOL_PIN);
   smoothing_value = SMOOTHING_RATIO * smoothing_value + (1 - SMOOTHING_RATIO) * (float)raw_value;
+}
 
+void show_sensor_value() {
   Serial.print("raw_value: ");
   Serial.print(raw_value);
   Serial.print(", ");
@@ -48,7 +51,9 @@ void loop() {
   Serial.print("smoothing_value: ");
   Serial.print(smoothing_value);
   Serial.print("\n");
+}
 
+void handle_sensor_value() {
   if (smoothing_value > 500) {
     if (is_led_on == 0) {
       change_to_on_color();
@@ -62,7 +67,12 @@ void loop() {
       M5.Lcd.print("none...\n");
     }
   }
+}
 
+void loop() {
+  update_sensor_value();
+  show_sensor_value();
+  handle_sensor_value();
   FastLED.show();
 
   delay(500);
