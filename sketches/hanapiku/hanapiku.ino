c136f82d65b38519a1295a5852e5e12e3343613c
daiking1756 <daiking1756@gmail.com>
2021-11-23 23:52:31 +0900

    Split some process to another method to make it better code

--- sketches/hanapiku/hanapiku.ino
+++ sketches/hanapiku/hanapiku.ino
@@ -1,5 +1,10 @@
 #include <M5StickC.h>
 #include <FastLED.h>
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiMulti.h>
+#include <HTTPClient.h>
+#include "secret.h"
 
 #define VOL_PIN 36
 #define LED_PIN 32
@@ -8,7 +13,7 @@
 
 int raw_value = 0;
 float smoothing_value = 0;
-int is_led_on = 0;
+int is_hanapiku_on = 0;
 CRGB leds_color[NUM_LEDS];
 
 void setup() {
@@ -53,18 +58,31 @@ void show_sensor_value() {
   Serial.print("\n");
 }
 
+void get_http_request() {
+  M5.Lcd.print("get_http_request() called\n");
+}
+
+void handle_hanapiku_on() {
+  is_hanapiku_on = 1;
+  change_to_on_color();
+  get_http_request();
+  M5.Lcd.print("HANAPIKU!\n");
+}
+
+void handle_hanapiku_off() {
+  is_hanapiku_on = 0;
+  change_to_off_color();
+  M5.Lcd.print("none...\n");
+}
+
 void handle_sensor_value() {
   if (smoothing_value > 500) {
-    if (is_led_on == 0) {
-      change_to_on_color();
-      is_led_on = 1;
-      M5.Lcd.print("HANAPIKU!\n");
+    if (is_hanapiku_on == 0) {
+      handle_hanapiku_on();
     }
   } else {
-    if (is_led_on == 1) {
-      change_to_off_color();
-      is_led_on = 0;
-      M5.Lcd.print("none...\n");
+    if (is_hanapiku_on == 1) {
+      handle_hanapiku_off();
     }
   }
 }
