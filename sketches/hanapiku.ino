5f2e9f0072acdde5f066f81b0359cb44316b5173
daiking1756 <daiking1756@gmail.com>
2021-11-23 15:57:02 +0900

    Migrate to M5StickC from M5Stack

--- sketches/hanapiku.ino
+++ sketches/hanapiku.ino
@@ -1,18 +1,11 @@
-#include <M5Stack.h>
-// アバターを表示するライブラリの読み込み
-#include <Avatar.h>
+#include <M5StickC.h>
 
-// 曲げセンサを35番ピンのADコンバータにつなぐ
-#define VOL_PIN 35
-
+// 曲げセンサを36番ピンのADコンバータにつなぐ
+#define VOL_PIN 36
+   
 // センサの値の平滑化に用いる定数
 //　0<a<1の範囲で設定する(大きいほど効果が大きくなる)
 #define a 0.0
-
-using namespace m5avatar;
-
-// Avatarクラスの変数定義
-Avatar avatar;
 
 // センサの値の変数定義
 int vol_value = 0;
@@ -20,15 +13,10 @@ float rc = 0;
 
 void setup() {
   M5.begin();
-  M5.Speaker.begin(); // これが無いとmuteしても無意味です。
-  M5.Speaker.mute();
   Serial.begin(115200);
 
-  // 35番ピンを入力モードにする
+  // 36番ピンを入力モードにする
   pinMode(VOL_PIN, INPUT);
-
-  // avatarオブジェクトを初期化
-  avatar.init();
 }
 
 void loop() {
@@ -50,14 +38,4 @@ void loop() {
 
   // 0.5秒に1回計測
   delay(500);
-
-  // 鼻がピクピクしたとき
-  if (rc > 350.00) { 
-    // 怒り顔の表示
-    avatar.setExpression(Expression::Angry);
-  } else { 
-    // ニコニコ顔の表示
-    avatar.setExpression(Expression::Happy);
-  }
-
 }
