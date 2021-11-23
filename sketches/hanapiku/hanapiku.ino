f6889e8bcbfd7bc55e62de7349147821a4b22ac6
daiking1756 <daiking1756@gmail.com>
2021-11-23 19:16:26 +0900

    Implement led_bar control

--- sketches/hanapiku/hanapiku.ino
+++ sketches/hanapiku/hanapiku.ino
@@ -1,15 +1,40 @@
 #include <M5StickC.h>
+#include <FastLED.h>
 
 #define VOL_PIN 36
+#define LED_PIN 32
 #define smoothing_ratio 0.1
+#define NUM_LEDS 3
 
 int raw_value = 0;
 float smoothing_value = 0;
+int is_led_on = 0;
+CRGB leds_color[NUM_LEDS];
 
 void setup() {
   M5.begin();
+  M5.Lcd.print("Hello HANAPIKU!\n\n");
   Serial.begin(115200);
   pinMode(VOL_PIN, INPUT);
+
+  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds_color, NUM_LEDS);
+  FastLED.setBrightness(20);
+
+  for(int i=0; i<NUM_LEDS; i++) {
+    leds_color[i] = CRGB::White;
+  }
+}
+
+void change_to_off_color() {
+  for(int i=0; i<NUM_LEDS; i++) {
+    leds_color[i] = CRGB::White;
+  }
+}
+
+void change_to_on_color() {
+  for(int i=0; i<NUM_LEDS; i++) {
+    leds_color[i] = CRGB::Red;
+  }
 }
 
 void loop() {
@@ -23,6 +48,22 @@ void loop() {
   Serial.print("smoothing_value: ");
   Serial.print(smoothing_value);
   Serial.print("\n");
+
+  if (smoothing_value > 500) {
+    if (is_led_on == 0) {
+      change_to_on_color();
+      is_led_on = 1;
+      M5.Lcd.print("HANAPIKU!\n");
+    }
+  } else {
+    if (is_led_on == 1) {
+      change_to_off_color();
+      is_led_on = 0;
+      M5.Lcd.print("none...\n");
+    }
+  }
+
+  FastLED.show();
 
   delay(500);
 }
