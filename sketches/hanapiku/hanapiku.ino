8a9a820e7ec78b8a03fb9c2e8d3cfe16ab307466
daiking1756 <daiking1756@gmail.com>
2021-11-24 09:52:17 +0900

    Impl calibration

--- sketches/hanapiku/hanapiku.ino
+++ sketches/hanapiku/hanapiku.ino
@@ -9,20 +9,23 @@
 
 #define VOL_PIN 36
 #define LED_PIN 32
-#define SMOOTHING_RATIO 0.1
+#define SMOOTHING_RATIO 0.0
 #define NUM_LEDS 3
+#define SAMPLING_NUM 10
+#define HANAPIKU_RATIO 1.5
 
 int raw_value = 0;
 float smoothing_value = 0;
-int is_hanapiku_on = 0;
+boolean is_hanapiku_on = false;
 CRGB leds_color[NUM_LEDS];
 WiFiMulti wifiMulti;
 HTTPClient http;
-//StaticJsonDocument<400> json_request_doc;
 char buffer[255];
-unsigned long counter = 0;
-unsigned long tick = 0;
 String authorization = "Bearer ";
+boolean is_calibration = false;
+int sampling_count = 0;
+int sampling_values[10];
+float base_value = 4096.0;
 
 void setup() {
   M5.begin();
@@ -57,21 +60,6 @@ void change_to_on_color() {
   }
 }
 
-void update_sensor_value() {
-  raw_value = analogRead(VOL_PIN);
-  smoothing_value = SMOOTHING_RATIO * smoothing_value + (1 - SMOOTHING_RATIO) * (float)raw_value;
-}
-
-void print_sensor_value() {
-  Serial.print("raw_value: ");
-  Serial.print(raw_value);
-  Serial.print(", ");
-
-  Serial.print("smoothing_value: ");
-  Serial.print(smoothing_value);
-  Serial.print("\n");
-}
-
 void post_line_message(String payload_text) {
   DynamicJsonDocument json_request_doc(200);
   JsonArray messages = json_request_doc.createNestedArray("messages");
@@ -83,55 +71,113 @@ void post_line_message(String payload_text) {
 
   serializeJson(json_request_doc, buffer, sizeof(buffer));
 
-//  M5.Lcd.print("POST begin...\n");
+  //  M5.Lcd.print("POST begin...\n");
 
-  http.begin("https://api.line.me/v2/bot/message/broadcast");
-  http.addHeader("Content-Type", "application/json");
-  http.addHeader("Authorization", authorization);
+   http.begin("https://api.line.me/v2/bot/message/broadcast");
+   http.addHeader("Content-Type", "application/json");
+   http.addHeader("Authorization", authorization);
 
-//  M5.Lcd.print("POST...\n");
-  int httpCode = http.POST((uint8_t*)buffer, strlen(buffer));  // start connection and send HTTP header.
-  if (httpCode > 0) { // httpCode will be negative on error.
-    Serial.printf("POST... code: %d\n", httpCode);
-//    M5.Lcd.print("See Serial...");
-    if (httpCode == HTTP_CODE_OK) { // file found at server.
-      Stream* resp = http.getStreamPtr();
+  //  M5.Lcd.print("POST...\n");
+   int httpCode = http.POST((uint8_t*)buffer, strlen(buffer));  // start connection and send HTTP header.
+   if (httpCode > 0) { // httpCode will be negative on error.
+     Serial.printf("POST... code: %d\n", httpCode);
+  //    M5.Lcd.print("See Serial...");
+     if (httpCode == HTTP_CODE_OK) { // file found at server.
+       Stream* resp = http.getStreamPtr();
 
-      DynamicJsonDocument json_response(255);
-      deserializeJson(json_response, *resp);
+       DynamicJsonDocument json_response(255);
+       deserializeJson(json_response, *resp);
 
-      serializeJson(json_response, Serial);
-      Serial.println("");
-    }
-  } else {
-    Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
-  }
-  http.end();
+       serializeJson(json_response, Serial);
+       Serial.println("");
+     }
+   } else {
+     Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
+   }
+   http.end();
+}
+
+void update_sensor_value() {
+  raw_value = analogRead(VOL_PIN);
+  smoothing_value = SMOOTHING_RATIO * smoothing_value + (1 - SMOOTHING_RATIO) * (float)raw_value;
+}
+
+void print_sensor_value() {
+  Serial.print("raw_value: ");
+  Serial.print(raw_value);
+  Serial.print(", ");
+
+  Serial.print("smoothing_value: ");
+  Serial.println(smoothing_value);
 }
 
 void handle_hanapiku_on() {
-  is_hanapiku_on = 1;
+  is_hanapiku_on = true;
   change_to_on_color();
   post_line_message("HANAPIKU <ON>");
   //  M5.Lcd.print("HANAPIKU!\n");
 }
 
 void handle_hanapiku_off() {
-  is_hanapiku_on = 0;
+  is_hanapiku_on = false;
   change_to_off_color();
-//  post_line_message("HANAPIKU <OFF>");
+  //  post_line_message("HANAPIKU <OFF>");
   //  M5.Lcd.print("none...\n");
 }
 
+boolean is_hanapikuing_now() {
+  if (smoothing_value / base_value > HANAPIKU_RATIO) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 void handle_sensor_value() {
-  if (smoothing_value > 500) {
-    if (is_hanapiku_on == 0) {
+  if (is_hanapikuing_now()) {
+    if (!is_hanapiku_on) {
       handle_hanapiku_on();
     }
   } else {
-    if (is_hanapiku_on == 1) {
+    if (is_hanapiku_on) {
       handle_hanapiku_off();
     }
+  }
+}
+
+void sensor_calibration() {
+  if (sampling_count > SAMPLING_NUM) {
+    int sampling_values_sum = 0;
+
+    for (int i = 0; i < SAMPLING_NUM; i++) {
+      sampling_values_sum += raw_value;
+    }
+    base_value = (float)sampling_values_sum / (float)SAMPLING_NUM;
+
+    is_calibration = false;
+    sampling_count = 0;
+    Serial.println("calibration completed");
+    Serial.print("base_value: ");
+    Serial.println(base_value);
+    Serial.println("");
+  } else {
+    sampling_values[sampling_count] = raw_value;
+
+    is_calibration = true;
+    sampling_count ++;
+    Serial.println("calibration now...");
+  }
+}
+
+
+void check_button() {
+  M5.update();
+  if ( M5.BtnA.pressedFor(3000) ) {
+    Serial.println("BtnA.pressedFor(3000) == TRUE");
+    is_calibration = true;
+  }
+  if (is_calibration) {
+    sensor_calibration();
   }
 }
 
@@ -140,6 +186,7 @@ void loop() {
   print_sensor_value();
   handle_sensor_value();
   FastLED.show();
+  check_button();
 
   delay(500);
 }
